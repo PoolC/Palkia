f926cc4011519644fa460b78753257b711c92d39
becooq81 <becooq81@gmail.com>
2023-08-01 11:33:10 +0900

    feat: comment/reply repositories

--- src/main/java/org/poolc/api/comment/repository/CommentRepository.java
+++ src/main/java/org/poolc/api/comment/repository/CommentRepository.java
@@ -1,2 +1,19 @@
-package org.poolc.api.comment.repository;public class CommentRepository {
+package org.poolc.api.comment.repository;
+
+import org.poolc.api.comment.domain.Comment;
+import org.poolc.api.post.domain.GeneralPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    @Query(value = "SELECT DISTINCT c FROM Comment c WHERE c.generalPost = :generalPost")
+    List<Comment> findAllByGeneralPost(@Param("generalPost") GeneralPost generalPost);
+
+    @Query(value = "SELECT DISTINCT c FROM Comment c WHERE c.parent = :parent")
+    List<Comment> findAllByParent(@Param("parent") Comment parent);
 }
--- src/main/java/org/poolc/api/comment/repository/ReplyRepository.java
+++ src/main/java/org/poolc/api/comment/repository/ReplyRepository.java
@@ -1,2 +1,15 @@
-package org.poolc.api.comment.repository;public interface ReplyRepository {
+package org.poolc.api.comment.repository;
+
+import org.poolc.api.comment.domain.Reply;
+import org.poolc.api.post.domain.QuestionPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReplyRepository extends JpaRepository<Reply, Long> {
+    @Query(value = "SELECT DISTINCT r FROM Reply r WHERE r.questionPost = :questionPost")
+    List<Reply> findAllByQuestionPost(QuestionPost questionPost);
 }
