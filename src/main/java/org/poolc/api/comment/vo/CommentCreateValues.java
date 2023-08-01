8c0968a8f8af22ce5ee718c81b74741c384d7428
becooq81 <becooq81@gmail.com>
2023-08-01 11:33:34 +0900

    feat: comment/reply vos

--- src/main/java/org/poolc/api/comment/vo/CommentCreateValues.java
+++ src/main/java/org/poolc/api/comment/vo/CommentCreateValues.java
@@ -1,2 +1,23 @@
-package org.poolc.api.comment.vo;public class CommentCreateValues {
+package org.poolc.api.comment.vo;
+
+import lombok.Getter;
+import org.poolc.api.comment.domain.Comment;
+import org.poolc.api.member.domain.Member;
+import org.poolc.api.post.domain.GeneralPost;
+
+@Getter
+public class CommentCreateValues {
+    private final GeneralPost generalPost;
+    private final Member member;
+    private final Boolean anonymous;
+    private final String body;
+    private final Comment parent;
+
+    public CommentCreateValues(GeneralPost generalPost, Member member, Boolean anonymous, String body, Comment parent) {
+        this.generalPost = generalPost;
+        this.member = member;
+        this.anonymous = anonymous;
+        this.body = body;
+        this.parent = parent;
+    }
 }
--- src/main/java/org/poolc/api/comment/vo/ReplyCreateValues.java
+++ src/main/java/org/poolc/api/comment/vo/ReplyCreateValues.java
@@ -1,2 +1,22 @@
-package org.poolc.api.comment.vo;public class ReplyCreateValues {
+package org.poolc.api.comment.vo;
+
+import lombok.Getter;
+import org.poolc.api.member.domain.Member;
+import org.poolc.api.post.domain.QuestionPost;
+
+@Getter
+public class ReplyCreateValues {
+    private final QuestionPost questionPost;
+    private final Member member;
+    private final Boolean anonymous;
+    private final String body;
+    private final Long likeCount;
+
+    public ReplyCreateValues(QuestionPost questionPost, Member member, Boolean anonymous, String body, Long likeCount) {
+        this.questionPost = questionPost;
+        this.member = member;
+        this.anonymous = anonymous;
+        this.body = body;
+        this.likeCount = likeCount;
+    }
 }
