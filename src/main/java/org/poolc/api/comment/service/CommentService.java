52ef684d3b8d249beea47b78c69bef51d417c7ee
becooq81 <becooq81@gmail.com>
2023-08-01 11:33:25 +0900

    feat: comment/reply services

--- src/main/java/org/poolc/api/comment/service/CommentService.java
+++ src/main/java/org/poolc/api/comment/service/CommentService.java
@@ -1,2 +1,66 @@
-package org.poolc.api.comment.service;public class CommentService {
+package org.poolc.api.comment.service;
+
+import lombok.RequiredArgsConstructor;
+import org.poolc.api.comment.domain.Comment;
+import org.poolc.api.comment.dto.CommentResponse;
+import org.poolc.api.comment.repository.CommentRepository;
+import org.poolc.api.comment.vo.CommentCreateValues;
+import org.poolc.api.post.domain.GeneralPost;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+    private final CommentRepository commentRepository;
+    public CommentResponse createComment(CommentCreateValues values) {
+        Comment comment = Comment.builder()
+                .generalPost(values.getGeneralPost())
+                .member(values.getMember())
+                .anonymous(values.getAnonymous())
+                .body(values.getBody())
+                .isDeleted(false)
+                .isChild(false)
+                .parent(null)
+                .build();
+        commentRepository.save(comment);
+        return CommentResponse.of(comment);
+    }
+
+    public CommentResponse createThread(CommentCreateValues values) {
+        Comment comment = Comment.builder()
+                .generalPost(values.getGeneralPost())
+                .member(values.getMember())
+                .anonymous(values.getAnonymous())
+                .body(values.getBody())
+                .isDeleted(false)
+                .isChild(true)
+                .parent(values.getParent())
+                .build();
+        commentRepository.save(comment);
+        return CommentResponse.of(comment);
+    }
+
+    public List<Comment> findCommentsByGeneralPost(GeneralPost generalPost) {
+        return commentRepository.findAllByGeneralPost(generalPost);
+    }
+
+    public List<Comment> findCommentsByParent(Long parentId) {
+        Comment parent = commentRepository.findById(parentId)
+                .orElseThrow(() -> new NoSuchElementException("No comment found with given comment id."));
+        return commentRepository.findAllByParent(parent);
+    }
+
+    public Comment findOne(Long commentId) {
+        return commentRepository.findById(commentId)
+                .orElseThrow(() -> new NoSuchElementException("No comment found with given comment id."));
+    }
+
+    public void deleteComment(Long commentId) {
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new NoSuchElementException("No comment found with given comment id."));
+        comment.setIsDeleted();
+    }
 }
--- src/main/java/org/poolc/api/comment/service/ReplyService.java
+++ src/main/java/org/poolc/api/comment/service/ReplyService.java
@@ -1,2 +1,57 @@
-package org.poolc.api.comment.service;public class ReplyService {
+package org.poolc.api.comment.service;
+
+import lombok.RequiredArgsConstructor;
+import org.poolc.api.comment.domain.Reply;
+import org.poolc.api.comment.dto.ReplyResponse;
+import org.poolc.api.comment.repository.ReplyRepository;
+import org.poolc.api.comment.vo.ReplyCreateValues;
+import org.poolc.api.post.domain.QuestionPost;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+@RequiredArgsConstructor
+public class ReplyService {
+    private final ReplyRepository replyRepository;
+
+    public ReplyResponse createReply(ReplyCreateValues values) {
+        Reply reply = Reply.builder()
+                .questionPost(values.getQuestionPost())
+                .member(values.getMember())
+                .anonymous(values.getAnonymous())
+                .body(values.getBody())
+                .isDeleted(false)
+                .likeCount(0L)
+                .build();
+        replyRepository.save(reply);
+        return ReplyResponse.of(reply);
+    }
+
+    public List<Reply> findRepliesByQuestionPost(QuestionPost questionPost) {
+        return replyRepository.findAllByQuestionPost(questionPost);
+    }
+
+    public Reply findOne(Long replyId) {
+        return replyRepository.findById(replyId)
+                .orElseThrow(() -> new NoSuchElementException("No reply found with given reply id."));
+    }
+
+    public void deleteReply(Long replyId) {
+        Reply reply = replyRepository.findById(replyId)
+                .orElseThrow(() -> new NoSuchElementException("No reply found with given reply id."));
+        reply.setIsDeleted();
+    }
+
+    public void likeReply(Long replyId) {
+        Reply reply = replyRepository.findById(replyId)
+                .orElseThrow(() -> new NoSuchElementException("No reply found with given reply id."));
+        reply.addLikeCount();
+    }
+    public void dislikeReply(Long replyId) {
+        Reply reply = replyRepository.findById(replyId)
+                .orElseThrow(() -> new NoSuchElementException("No reply found with given reply id."));
+        reply.deductLikeCount();
+    }
 }
