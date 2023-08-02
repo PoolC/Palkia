c7350bce84b5d1ce1c84ae5861a26353d8826ecf
becooq81 <becooq81@gmail.com>
2023-08-02 13:43:53 +0900

    feat(Comment): 권한 설정

--- src/main/java/org/poolc/api/comment/service/CommentService.java
+++ src/main/java/org/poolc/api/comment/service/CommentService.java
@@ -1,11 +1,13 @@
 package org.poolc.api.comment.service;
 
 import lombok.RequiredArgsConstructor;
+import org.poolc.api.auth.exception.UnauthorizedException;
 import org.poolc.api.comment.domain.Comment;
 import org.poolc.api.comment.dto.CommentResponse;
 import org.poolc.api.comment.repository.CommentRepository;
 import org.poolc.api.comment.vo.CommentCreateValues;
 import org.poolc.api.comment.vo.CommentUpdateValues;
+import org.poolc.api.member.domain.Member;
 import org.poolc.api.post.domain.Post;
 import org.springframework.stereotype.Service;
 
@@ -42,24 +44,40 @@ public class CommentService {
                 .orElseThrow(() -> new NoSuchElementException("No comment found with given comment id."));
     }
 
-    public void deleteComment(Long commentId) {
+    public void deleteComment(Member member, Long commentId) {
         Comment comment = findById(commentId);
+        checkWriterOrAdmin(member, comment);
         comment.setIsDeleted();
         comment.getPost().deductCommentCount();
     }
 
-    public void updateComment(Long commentId, CommentUpdateValues commentUpdateValues) {
+    public void updateComment(Member member, Long commentId, CommentUpdateValues commentUpdateValues) {
         Comment comment = findById(commentId);
+        checkWriter(member, comment);
         comment.updateComment(commentUpdateValues);
     }
 
-    public void likeComment(Long commentId) {
+    public void likeComment(Member member, Long commentId) {
         Comment comment = findById(commentId);
+        checkNotWriter(member, comment);
         if (comment.getPost().getIsQuestion()) comment.addLikeCount();
     }
 
-    public void dislikeComment(Long commentId) {
+    public void dislikeComment(Member member, Long commentId) {
         Comment comment = findById(commentId);
+        checkNotWriter(member, comment);
         if (comment.getPost().getIsQuestion()) comment.deductLikeCount();
+    }
+
+    private void checkWriterOrAdmin(Member member, Comment comment) {
+        if (!comment.getMember().equals(member) && !member.isAdmin()) throw new UnauthorizedException("접근할 수 없습니다.");
+    }
+
+    private void checkWriter(Member member, Comment comment) {
+        if (!comment.getMember().equals(member)) throw new UnauthorizedException("접근할 수 없습니다.");
+    }
+
+    private void checkNotWriter(Member member, Comment comment) {
+        if (comment.getMember().equals(member)) throw new UnauthorizedException("자신의 댓글을 좋아할 수 없습니다.");
     }
 }
