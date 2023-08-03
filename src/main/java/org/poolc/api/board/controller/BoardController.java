0ac94de68c9ee9840c806d5983914b0a1494411c
becooq81 <becooq81@gmail.com>
2023-08-03 16:29:25 +0900

    feat: board controller

--- src/main/java/org/poolc/api/board/controller/BoardController.java
+++ src/main/java/org/poolc/api/board/controller/BoardController.java
@@ -1,30 +1,99 @@
 package org.poolc.api.board.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.poolc.api.auth.exception.UnauthorizedException;
+import org.poolc.api.board.domain.Board;
 import org.poolc.api.board.dto.BoardCreateRequest;
 import org.poolc.api.board.dto.BoardResponse;
+import org.poolc.api.board.dto.BoardUpdateRequest;
 import org.poolc.api.board.service.BoardService;
 import org.poolc.api.board.vo.BoardCreateValues;
+import org.poolc.api.board.vo.BoardUpdateValues;
 import org.poolc.api.member.domain.Member;
+import org.poolc.api.member.domain.MemberRole;
+import org.poolc.api.member.domain.MemberRoles;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/boards")
+@RequestMapping("/board")
 public class BoardController {
     private final BoardService boardService;
 
     @PostMapping("/new")
     public ResponseEntity<?> registerBoard(@RequestBody @Valid BoardCreateRequest request) {
         boardService.createBoard(new BoardCreateValues(request));
-        return ResponseEntity.ok().build();
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
+    @GetMapping
+    public ResponseEntity<List<BoardResponse>> viewBoardList(@AuthenticationPrincipal Member member) {
+        List<Board> boards = boardService.findAll();
+        List<BoardResponse> boardResponseList;
+        if (member == null) {
+            boardResponseList = boards.stream()
+                    .filter(Board::isPublicReadPermission)
+                    .map(BoardResponse::of)
+                    .collect(Collectors.toList());
+        } else {
+            boardResponseList = boards.stream()
+                    .filter(board -> board.memberHasReadPermissions(member.getRoles()))
+                    .map(BoardResponse::of)
+                    .collect(Collectors.toList());
+        }
+        boardResponseList.sort(Comparator.comparing(BoardResponse::getBoardId));
+        return ResponseEntity.status(HttpStatus.OK).body(boardResponseList);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BoardResponse> viewBoard(@AuthenticationPrincipal Member member, @PathVariable Long id) {
+        Board board = boardService.findById(id);
+
+        if (member == null) {
+            checkReadPermissions(board, new MemberRoles(Set.of(MemberRole.PUBLIC)));
+        } else {
+            checkReadPermissions(board, member.getRoles());
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(BoardResponse.of(board));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<BoardResponse> updateBoard(@AuthenticationPrincipal Member member,
+                                                     @PathVariable Long id,
+                                                     @RequestBody BoardUpdateRequest request) {
+        Board board = boardService.findById(id);
+        checkWritePermissions(board, member);
+        boardService.updateBoard(id, new BoardUpdateValues(request));
+        return ResponseEntity.status(HttpStatus.OK).body(BoardResponse.of(board));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBoard(@AuthenticationPrincipal Member member, @PathVariable Long id) {
+        Board board = boardService.findById(id);
+        checkWritePermissions(board, member);
+        boardService.deleteBoard(id);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    private void checkReadPermissions(Board board, MemberRoles roles) {
+        if (!board.memberHasReadPermissions(roles)) {
+            throw new UnauthorizedException("접근할 수 없습니다");
+        }
+    }
+    private void checkWritePermissions(Board board, Member member) {
+        if (!board.memberHasWritePermissions(member)) {
+            throw new UnauthorizedException("접근할 수 없습니다");
+        }
+    }
+
 }
