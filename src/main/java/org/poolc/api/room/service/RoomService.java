01c25f47b324d96532254f45c9edce878d1034b0
jimmy0006 <jinmin645@gmail.com>
2023-08-04 14:10:41 +0900

    feat:error handling in room reservation

--- src/main/java/org/poolc/api/room/service/RoomService.java
+++ src/main/java/org/poolc/api/room/service/RoomService.java
@@ -1,8 +1,11 @@
 package org.poolc.api.room.service;
 
 import lombok.RequiredArgsConstructor;
+import org.poolc.api.auth.exception.UnauthorizedException;
 import org.poolc.api.member.domain.Member;
 import org.poolc.api.room.domain.Room;
+import org.poolc.api.room.exception.BadRequestException;
+import org.poolc.api.room.exception.ForbiddenException;
 import org.poolc.api.room.repository.RoomRepository;
 import org.poolc.api.room.vo.RoomReservation;
 import org.poolc.api.room.vo.RoomReservationSearch;
@@ -12,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
@@ -43,7 +47,7 @@ public class RoomService {
 
     @Transactional
     public void updateReservation(RoomReservation reservation, Long id){
-        Room room = roomRepository.findRoomReservationById(id).orElseThrow(() -> new RuntimeException("해당하는 id의 예약이 없습니다."));
+        Room room = roomRepository.findRoomReservationById(id).orElseThrow(() -> new NoSuchElementException("해당하는 예약이 존재하지 않습니다."));
         //check host
         if(reservation.getHost().equals(room.getHost())){
             RequestValidCheck(reservation);
@@ -51,27 +55,27 @@ public class RoomService {
             room.editRoom(reservation.getDate(),reservation.getStart(),reservation.getEnd(),reservation.getPurpose());
             roomRepository.save(room);
         }else{
-            throw new RuntimeException("내가 한 예약이 아닙니다.");
+            throw new ForbiddenException("내가 한 예약이 아닙니다.");
         }
     }
 
     public void cancelReservation(Member member, Long reservationId){
-        Room room = roomRepository.findRoomReservationById(reservationId).orElseThrow(() -> new RuntimeException("해당하는 예약이 존재하지 않습니다."));
+        Room room = roomRepository.findRoomReservationById(reservationId).orElseThrow(() -> new NoSuchElementException("해당하는 예약이 존재하지 않습니다."));
         if(member.isAdmin() || member.equals(room.getHost())){
             // 삭제
             roomRepository.delete(room);
         }else{
-            throw new RuntimeException("내가 한 예약이 아닙니다.");
+            throw new ForbiddenException("내가 한 예약이 아닙니다.");
         }
 
     }
 
     private void RequestValidCheck(RoomReservation roomReservation){
         if(roomReservation.getStart().isAfter(roomReservation.getEnd())){
-            throw new RuntimeException("입력이 잘못되었습니다.");
+            throw new BadRequestException("입력이 잘못되었습니다.");
         }
         if(roomReservation.getStart().getMinute()%30!=0 || roomReservation.getEnd().getMinute()%30!=0){
-            throw new RuntimeException("30분 단위로만 예약할 수 있습니다.");
+            throw new BadRequestException("30분 단위로만 예약할 수 있습니다.");
         }
     }
 
@@ -81,7 +85,7 @@ public class RoomService {
 //        }
 //        roomRepository.validCheck(roomReservation.getDate(), roomReservation.getStart(), roomReservation.getEnd()).ifPresent(a->{throw new RuntimeException("해당 시간에 예약이 존재합니다.");});
         if(roomRepository.validCheck(roomReservation.getDate(), roomReservation.getStart(), roomReservation.getEnd())>0){
-            throw new RuntimeException("해당 시간에 예약이 존재합니다.");
+            throw new BadRequestException("해당 시간에 예약이 존재합니다.");
         }
 
     }
