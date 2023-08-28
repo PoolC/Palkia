691c1dc57024a70c80f6e0bd4ea6858d7c2374b5
becooq81 <becooq81@gmail.com>
2023-08-29 02:28:18 +0900

    feat: board name

--- src/main/java/org/poolc/api/board/domain/BoardName.java
+++ src/main/java/org/poolc/api/board/domain/BoardName.java
@@ -1,4 +1,14 @@
 package org.poolc.api.board.domain;
 
 public enum BoardName {
+    MAIN("자유게시판"),
+    JOB("취업합시다"),
+    CS("CS 게시판"),
+    PROJECT("사람 구해요~");
+
+    private final String description;
+
+    private BoardName(String description) { this.description = description; }
+
+    public String getDescription() { return this.description; }
 }
