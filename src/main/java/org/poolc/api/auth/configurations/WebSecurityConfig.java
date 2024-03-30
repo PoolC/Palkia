7c075b9817025d11a564fcb68eeb96b18382e808
jimmy0006 <jinmin645@gmail.com>
2024-03-31 02:47:16 +0900

    for problem searching

--- src/main/java/org/poolc/api/auth/configurations/WebSecurityConfig.java
+++ src/main/java/org/poolc/api/auth/configurations/WebSecurityConfig.java
@@ -65,7 +65,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .cors().and()
+                .cors().disable()
                 .httpBasic().disable()
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
