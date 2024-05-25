ffa87998f2fd9d2080e868a6cd420fe5da2419b5
becooq81 <becooq81@gmail.com>
2024-05-25 18:52:01 +0900

    fix: @transactional for dirty checking

--- src/main/java/org/poolc/api/notification/service/NotificationService.java
+++ src/main/java/org/poolc/api/notification/service/NotificationService.java
@@ -1,5 +1,6 @@
 package org.poolc.api.notification.service;
 
+import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.poolc.api.member.domain.Member;
 import org.poolc.api.member.repository.MemberRepository;
@@ -20,18 +21,19 @@ public class NotificationService {
     private final NotificationRepository notificationRepository;
     private final MemberRepository memberRepository;
 
+    @Transactional
     public List<Notification> getUnreadNotificationsForMember(String receiverId) {
         List<Notification> notifications = notificationRepository.findByReceiverIdAndReadStatus(receiverId, false)
                 .stream()
                 .sorted(Comparator.comparing(Notification::getCreatedAt).reversed())
                 .collect(Collectors.toList());
         notifications.forEach(Notification::memberReads);
-        notificationRepository.saveAll(notifications);
         Member recipient = getMemberByLoginID(receiverId);
         recipient.resetNotificationCount();
         return notifications;
     }
 
+    @Transactional
     public List<Notification> getAllNotificationsForMember(String receiverId) {
         List<Notification> notifications = notificationRepository.findAllByReceiverId(receiverId)
                 .stream()
@@ -43,11 +45,13 @@ public class NotificationService {
         return notifications;
     }
 
+    @Transactional
     public void createBadgeNotification(Member receiver) {
         notificationRepository.save(new Notification(receiver.getLoginID(), NotificationType.BADGE));
         receiver.addNotification();
     }
 
+    @Transactional
     public void createMessageNotification(String senderId, String receiverId) {
         Member sender = getMemberByLoginID(senderId);
         Member receiver = getMemberByLoginID(receiverId);
@@ -55,6 +59,7 @@ public class NotificationService {
         receiver.addNotification();
     }
 
+    @Transactional
     public void createCommentNotification(String senderId, String receiverId, Long postId) {
         Member sender = getMemberByLoginID(senderId);
         Member receiver = getMemberByLoginID(receiverId);
@@ -62,6 +67,7 @@ public class NotificationService {
         receiver.addNotification();
     }
 
+    @Transactional
     public void createRecommentNotification(String senderId, String receiverId, Long postId, Long parentCommentId) {
         Member sender = getMemberByLoginID(senderId);
         Member receiver = getMemberByLoginID(receiverId);
