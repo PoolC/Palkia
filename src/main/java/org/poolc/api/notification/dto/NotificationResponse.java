0e92aad38dd15831ae015911639d05e5cdf65732
becooq81 <becooq81@gmail.com>
2024-05-26 17:27:17 +0900

    fix: user IDs are not necessary in notification response

--- src/main/java/org/poolc/api/notification/dto/NotificationResponse.java
+++ src/main/java/org/poolc/api/notification/dto/NotificationResponse.java
@@ -11,16 +11,12 @@ import java.time.LocalDateTime;
 @Getter
 @Setter(AccessLevel.PRIVATE)
 public class NotificationResponse {
-    private String receiverId;
     private Boolean readStatus;
     private String notificationType;
     private LocalDateTime createdAt;
-
     private String senderName;
-    private String senderId;
     // 댓글 알림이면 포스트로 링크
     private Long postId;
-
     // 대댓글이면 해당 댓글 주인에게 알림 보내기 위해
     private Long parentCommentId;
 
@@ -29,10 +25,8 @@ public class NotificationResponse {
         response.setNotificationType(notification.getNotificationType().getDescription());
         response.setReadStatus(notification.getReadStatus());
         response.setCreatedAt(notification.getCreatedAt());
-        response.setReceiverId(notification.getReceiverId());
 
         if (notification.getNotificationType() != NotificationType.BADGE) {
-            response.setSenderId(notification.getSenderId());
             response.setSenderName(notification.getSenderName());
             if (notification.getNotificationType() == NotificationType.COMMENT) {
                 response.setPostId(notification.getPostId());
