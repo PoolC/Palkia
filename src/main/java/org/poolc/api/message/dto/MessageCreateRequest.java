c65cecfa3a6ec8fc41f7e2d1626563d03ed666e3
becooq81 <becooq81@gmail.com>
2024-05-26 17:25:28 +0900

    refactor: remove unnecssary fields in message create request

--- src/main/java/org/poolc/api/message/dto/MessageCreateRequest.java
+++ src/main/java/org/poolc/api/message/dto/MessageCreateRequest.java
@@ -1,22 +1,17 @@
 package org.poolc.api.message.dto;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 @Getter
 public class MessageCreateRequest {
     private final String content;
-    private final String senderId;
-    private final String receiverId;
-    private final Boolean senderAnonymous;
-    private final Boolean receiverAnonymous;
+    private final String conversationId;
 
     @JsonCreator
-    public MessageCreateRequest(String content, String senderId, String receiverId, Boolean senderAnonymous, Boolean receiverAnonymous) {
+    public MessageCreateRequest(@JsonProperty("content") String content, @JsonProperty("conversationId") String conversationId) {
         this.content = content;
-        this.senderId = senderId;
-        this.receiverId = receiverId;
-        this.senderAnonymous = senderAnonymous;
-        this.receiverAnonymous = receiverAnonymous;
+        this.conversationId = conversationId;
     }
 }
