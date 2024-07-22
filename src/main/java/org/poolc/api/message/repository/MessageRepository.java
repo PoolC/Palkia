fb4ac668f82dccc2e96e05bed278005bc7b094a2
jinny <becooq81@gmail.com>
2024-07-22 12:09:33 +0900

    style: reflect changes from conversation id renaming

--- src/main/java/org/poolc/api/message/repository/MessageRepository.java
+++ src/main/java/org/poolc/api/message/repository/MessageRepository.java
@@ -1,6 +1,8 @@
 package org.poolc.api.message.repository;
 
 import java.util.Optional;
+
+import org.poolc.api.conversation.domain.Conversation;
 import org.poolc.api.message.domain.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,8 +13,8 @@ import java.util.List;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
-    @Query("select m from Message m where m.conversation.id=(:id)")
-    List<Message> findAllByConversationID(@Param("id") String conversationId);
+
+    List<Message> findAllByConversationId(String conversationID);
 
     @Query("SELECT m FROM Message m WHERE m.conversation.id = :conversationId ORDER BY m.createdAt DESC")
     Optional<Message> findTopByConversationIdOrderByCreatedAtDesc(@Param("conversationId") String conversationId);
