532b4b55f547d07d0fe1bb90615056d2a1a28d46
jinny <becooq81@gmail.com>
2024-07-25 08:29:46 +0900

    refactor: remove unnecessary imports & methods

--- src/main/java/org/poolc/api/message/repository/MessageRepository.java
+++ src/main/java/org/poolc/api/message/repository/MessageRepository.java
@@ -1,12 +1,7 @@
 package org.poolc.api.message.repository;
 
-import java.util.Optional;
-
-import org.poolc.api.conversation.domain.Conversation;
 import org.poolc.api.message.domain.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,8 +10,4 @@ import java.util.List;
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
     List<Message> findAllByConversationId(String conversationID);
-
-    @Query("SELECT m FROM Message m WHERE m.conversation.id = :conversationId ORDER BY m.createdAt DESC")
-    Optional<Message> findTopByConversationIdOrderByCreatedAtDesc(@Param("conversationId") String conversationId);
-
 }
