3154abdf0caf9f522f8e7b332ed3bf680bfd0369
jinny <becooq81@gmail.com>
2024-07-25 09:10:40 +0900

    fix: exclude deleted (though exists in database) posts from retrieval

--- src/main/java/org/poolc/api/post/repository/PostRepository.java
+++ src/main/java/org/poolc/api/post/repository/PostRepository.java
@@ -14,10 +14,13 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long>, PagingAndSortingRepository<Post, Long>, JpaSpecificationExecutor<Post> {
+    @Query("select p from Post p where p.member=:member and p.isDeleted=false")
     Page<Post> findByMember(Member member, Pageable pageable);
 
     @Query("select p from Post p where p.boardType=:boardType and p.isDeleted=false")
     Page<Post> findByBoardType(@Param("boardType") BoardType boardType, Pageable pageable);
+
+    @Query("select p from Post p where (p.title like %:title% or p.body like %:body%) and p.isDeleted = false")
     Page<Post> findByTitleContainingOrBodyContaining(String title, String body, Pageable pageable);
 
 }
