81278a1598e5b09a74213e8fc6d15570f7278c6b
becooq81 <becooq81@gmail.com>
2024-08-06 21:03:27 +0900

    fix(NotificationRepository): added @Param for parameters for repository methods

--- src/main/java/org/poolc/api/notification/repository/NotificationRepository.java
+++ src/main/java/org/poolc/api/notification/repository/NotificationRepository.java
@@ -6,13 +6,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.parameters.P;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
-    List<Notification> findByReceiverIdAndReadStatus(String receiverId, Boolean readStatus);
+    @Query("SELECT n FROM Notification n WHERE n.receiverId = :receiverId AND n.readStatus = :readStatus")
+    List<Notification> findByReceiverIdAndReadStatus(@Param("receiverId") String receiverId, @Param("readStatus") Boolean readStatus);
 
-    List<Notification> findAllByReceiverId(String receiverId);
+    @Query("SELECT n FROM Notification n WHERE n.receiverId = :receiverId ORDER BY n.createdAt DESC")
+    List<Notification> findAllByReceiverId(@Param("receiverId") String receiverId);
 
     @Query("SELECT COUNT(n) FROM Notification n WHERE n.receiverId = :receiverId AND n.readStatus = false")
-    Long countByReceiverIdAndReadIsFalse(String receiverId);
+    Long countByReceiverIdAndReadIsFalse(@Param("receiverId") String receiverId);
 }
