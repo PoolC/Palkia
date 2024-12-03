f6823f243511b4d756463e31b54699e583d3cfaf
becooq81 <becooq81@gmail.com>
2024-12-04 00:38:58 +0900

    feat: add create book request

--- src/main/java/org/poolc/api/book/dto/request/CreateBookRequest.java
+++ src/main/java/org/poolc/api/book/dto/request/CreateBookRequest.java
@@ -1,4 +1,41 @@
 package org.poolc.api.book.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateBookRequest {
+
+    @Length(min = 1, max = 100)
+    private String title;
+
+    @Length(min = 1, max = 100)
+    private String author;
+
+    @Length(min = 1, max = 100)
+    private String publisher;
+
+    @Length(min = 1, max = 100)
+    private String isbn;
+
+    @Length(min = 1)
+    private String description;
+
+    @Length(min = 1, max = 100)
+    private String pubdate;
+
+    @Length(min = 1)
+    private String image;
+
+    @NotNull
+    private Integer discount;
+
+    @Length(min = 1)
+    private String link;
 }
