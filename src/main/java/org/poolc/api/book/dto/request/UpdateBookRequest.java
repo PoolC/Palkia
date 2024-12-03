5b1a22166edbffd805279d351ba7d8d90655f9d2
becooq81 <becooq81@gmail.com>
2024-12-04 00:39:07 +0900

    feat: add update book request

--- src/main/java/org/poolc/api/book/dto/request/UpdateBookRequest.java
+++ src/main/java/org/poolc/api/book/dto/request/UpdateBookRequest.java
@@ -1,4 +1,22 @@
 package org.poolc.api.book.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UpdateBookRequest {
+
+    private String title;
+    private String author;
+    private String publisher;
+    private String isbn;
+    private String description;
+    private String pubdate;
+    private String image;
+    private Integer discount;
+    private String link;
+
 }
