4e20b020e8808898efe6c1ee662eb529e082d895
becooq81 <becooq81@gmail.com>
2024-12-03 22:58:03 +0900

    feat: add payload to contain individual book api response

--- src/main/java/org/poolc/api/book/dto/BookApiResponse.java
+++ src/main/java/org/poolc/api/book/dto/BookApiResponse.java
@@ -1,4 +1,39 @@
 package org.poolc.api.book.dto;
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookApiResponse {
+
+    @JacksonXmlProperty(localName = "title")
+    private String title;
+
+    @JacksonXmlProperty(localName = "link")
+    private String link;
+
+    @JacksonXmlProperty(localName = "image")
+    private String image;
+
+    @JacksonXmlProperty(localName = "author")
+    private String author;
+
+    @JacksonXmlProperty(localName = "discount")
+    private Integer discount;
+
+    @JacksonXmlProperty(localName = "publisher")
+    private String publisher;
+
+    @JacksonXmlProperty(localName = "isbn")
+    private String isbn;
+
+    @JacksonXmlProperty(localName = "description")
+    private String description;
+
+    @JacksonXmlProperty(localName = "pubdate")
+    private String pubdate;
+
 }
