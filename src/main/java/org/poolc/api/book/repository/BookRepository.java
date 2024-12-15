99a9f2a51aab7903ed9ccd3441d90ba460f3c74f
becooq81 <becooq81@gmail.com>
2024-12-15 23:24:29 +0900

    feat(repo): add find all methods with sorting options

--- src/main/java/org/poolc/api/book/repository/BookRepository.java
+++ src/main/java/org/poolc/api/book/repository/BookRepository.java
@@ -6,11 +6,19 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     boolean existsByTitleAndAuthor(String title, String author);
 
     Optional<Book> findBookById(Long id);
     Page<Book> findAllByOrderByCreatedAtDesc(Pageable pageable);
+    Page<Book> findAllByOrderByTitleAsc(Pageable pageable);
+
+    @Query("SELECT b FROM Book b " +
+            "ORDER BY CASE WHEN b.rentDate IS NULL THEN 0 ELSE 1 END, " +
+            "b.rentDate DESC, " +
+            "b.title ASC")
+    Page<Book> findAllByOrderByRentDateDescTitleAsc(Pageable pageable);
 
 }
