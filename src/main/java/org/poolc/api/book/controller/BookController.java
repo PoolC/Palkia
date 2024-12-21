ff2a37062944b92d290adb308dd1b7a78a647804
jimmy0006 <jinmin645@gmail.com>
2024-12-22 00:21:10 +0900

    fix:add data type and returns null when error

--- src/main/java/org/poolc/api/book/controller/BookController.java
+++ src/main/java/org/poolc/api/book/controller/BookController.java
@@ -5,8 +5,11 @@ import org.poolc.api.book.client.BookClient;
 import org.poolc.api.book.domain.BookSortOption;
 import org.poolc.api.book.dto.request.CreateBookRequest;
 import org.poolc.api.book.dto.request.UpdateBookRequest;
+import org.poolc.api.book.dto.response.BookApiResponse;
+import org.poolc.api.book.dto.response.BookResponse;
 import org.poolc.api.book.service.BookService;
 import org.poolc.api.member.domain.Member;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
+import java.util.List;
 
 @RestController
 @RequestMapping("/book")
@@ -24,85 +28,85 @@ public class BookController {
     private final BookService bookService;
 
     @GetMapping("/search")
-    public ResponseEntity<?> searchBooks(@RequestParam String query,
-                                         @RequestParam(value = "page", defaultValue = "0") @Min(0) Integer page) {
+    public ResponseEntity<List<BookApiResponse>> searchBooks(@RequestParam String query,
+                                                             @RequestParam(value = "page", defaultValue = "0") @Min(0) Integer page) {
         try {
             return new ResponseEntity<>(bookClient.searchBooks(query, page), HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @GetMapping("/all")
-    public ResponseEntity<?> getAllBooks(
+    public ResponseEntity<Page<BookResponse>> getAllBooks(
             @RequestParam(value = "page", defaultValue = "0") @Min(0) Integer page,
             @RequestParam(value = "sort", required = false) BookSortOption sortOption) {
         try {
             return new ResponseEntity<>(bookService.getAllBooks(page, sortOption), HttpStatus.OK);
         } catch (Exception e) {
             e.printStackTrace();
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getBook(@PathVariable Long id) {
+    public ResponseEntity<BookResponse> getBook(@PathVariable Long id) {
         try {
             return new ResponseEntity<>(bookService.getBook(id), HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @PostMapping("/new")
-    public ResponseEntity<?> addBook(@AuthenticationPrincipal Member member,
+    public ResponseEntity<Void> addBook(@AuthenticationPrincipal Member member,
                                      @Valid @RequestBody CreateBookRequest request) {
         try {
             bookService.createBook(member, request);
             return new ResponseEntity<>(HttpStatus.CREATED);
         } catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteBook(@AuthenticationPrincipal Member member, @PathVariable Long id) {
+    public ResponseEntity<Void> deleteBook(@AuthenticationPrincipal Member member, @PathVariable Long id) {
         try {
             bookService.deleteBook(member, id);
             return new ResponseEntity<>(HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<?> updateBook(@AuthenticationPrincipal Member member, @PathVariable Long id,
+    public ResponseEntity<Void> updateBook(@AuthenticationPrincipal Member member, @PathVariable Long id,
                                         @Valid @RequestBody UpdateBookRequest request) {
         try {
             bookService.updateBook(member, id, request);
             return new ResponseEntity<>(HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @PostMapping("/{id}/borrow")
-    public ResponseEntity<?> borrowBook(@AuthenticationPrincipal Member member, @PathVariable Long id) {
+    public ResponseEntity<Void> borrowBook(@AuthenticationPrincipal Member member, @PathVariable Long id) {
         try {
             bookService.rent(member, id);
             return new ResponseEntity<>(HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @PostMapping("/{id}/return")
-    public ResponseEntity<?> returnBook(@AuthenticationPrincipal Member member, @PathVariable Long id) {
+    public ResponseEntity<Void> returnBook(@AuthenticationPrincipal Member member, @PathVariable Long id) {
         try {
             bookService.returnBook(member, id);
             return new ResponseEntity<>(HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
